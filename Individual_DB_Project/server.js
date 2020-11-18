34a96b744aefdac7ae913b00937d52dc28d07db5
bjaynes01 <55394496+bjaynes01@users.noreply.github.com>
2020-11-18 17:09:07 -0600

    this is what is here so far

---
+++ Individual_DB_Project/server.js
@@ -0,0 +1,310 @@
+const express = require('express');
+
+const bodyParser = require('body-parser')
+
+const mysql = require('mysql');
+const { query } = require('express');
+
+const app = express();
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Base2018",
+    database: "baseballstore"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query('SELECT * FROM products', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+      });
+  });
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/AdminPage', (req, res) =>{
+    res.render('AdminHome');
+});
+
+app.get('/addCustomer', (req, res) => {
+    res.render('newCustomer');
+});
+
+app.post('/addCustomerAction', (req, res) => {
+    console.log('Got body:', req.body);
+    var str = "INSERT INTO `baseballstore`.`customers` (`First_Name`, `Last_Name`, `Gender`, `Email`) VALUES ('"+ req.body.Fname +"', '" + req.body.Lname + "', '"+ req.body.Gender +"', '" + req.body.email + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/ManageCustomers');
+});
+
+app.get('/addEmployee', (req, res) => {
+    res.render('newEmployee');
+});
+
+app.post('/addEmployeeAction', (req, res) => {
+    console.log('Got body:', req.body);
+    var str = "INSERT INTO `employees` (`First_Name`, `Last_Name`, `Gender`, `Email`, `Salary`) VALUES (" + "'" +  req.body.fname + "', '" + req.body.Lname + "', '" + req.body.Gender + "', '" +req.body.email + "', '" + req.body.salary + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/ManageEmployees');
+});
+
+app.post('/addOrder', (req, res) =>{
+    console.log('Got body:', req.body);
+    res.render('newSale');
+});
+
+app.post('/addOrderAction', (req, res) => {
+    console.log('Got body:', req.body);
+    res.redirect('/Customer');
+});
+
+app.get('/addProduct', (req, res) => {
+    res.render('newProduct');
+});
+
+app.post('/addProductAction', (req, res) => {
+    console.log('Got body:', req.body);
+    var str = "INSERT INTO `baseballstore`.`products` (`P_name`, `type`, `stock`, `price`) VALUES ('" + req.body.name + "', '"+ req.body.type +"', '" + req.body.count + "', '" + req.body.price + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/ManageProducts');
+});
+
+app.get('/addSteamerOrder', (req, res) => {
+    res.render('newSteamerOrder');
+});
+
+app.post('/addSteamerOrderAction', (req, res) => {
+    console.log('Got body:', req.body);
+    var str = "INSERT INTO `baseballstore`.`glove_steaming` (`customer_ID`, `employee_ID`, `price`) VALUES ('" + req.body.CusID + "', '" + req.body.EmpID + "', '" + req.body.price + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/Manage_Steam_Orders');
+});
+
+app.get('/Customer', (req, res) =>{
+    con.query('SELECT * FROM products', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('CustomerPage', {"products": rows});
+    });
+});
+
+app.post('/deleteCustomer', (req, res) =>{
+    console.log('Got body:', req.body);
+    var str = "DELETE FROM `baseballstore`.`customers` WHERE (`Cus_ID` = '" + req.body.dropDown + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/ManageCustomers');
+});
+
+app.post('/deleteEmployee', (req, res) =>{
+    console.log('Got body:', req.body);
+    var str = "DELETE FROM `baseballstore`.`employees` WHERE (`Emp_ID` = '" + req.body.dropDown + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/ManageEmployees');
+});
+
+app.post('/deleteProduct', (req, res) =>{
+    console.log('Got body:', req.body);
+    var str = "DELETE FROM `baseballstore`.`products` WHERE (`product_ID` = '" + req.body.dropDown + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/ManageProducts');
+});
+
+app.post('/deleteSteamerOrder', (req, res) =>{
+    console.log('Got body:', req.body);
+    var str = "DELETE FROM `baseballstore`.`glove_steaming` WHERE (`glove_steaming_ID` = '" + req.body.dropDown + "')";
+    console.log(str);
+    con.query(str);
+    res.redirect('/Manage_Steam_Orders');
+});
+
+app.get('/Home', (req, res) => {
+    res.render('HomePage');
+});
+
+app.get('/Login', (req, res) => {
+    res.render('Login');
+});
+
+app.get('/Logout', (req, res) => {
+    res.render('Login');
+});
+
+app.get('/ManageCustomers', (req, res) => {
+    con.query('SELECT * FROM customers', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('Manage_Cus', {"customers": rows});
+    });
+});
+
+app.get('/ManageEmployees', (req, res) => {
+    con.query('SELECT * FROM employees', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('Manage_Emp', {"employees": rows});
+    });
+});
+
+app.get('/ManageProducts', (req, res) => {
+    con.query('SELECT * FROM products', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('Manage_Product', {"products": rows});
+    });
+});
+
+app.get('/Manage_Steam_Orders', (req, res) => {
+    con.query('SELECT * FROM glove_steaming', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('Manage_Steam_Orders', {"steams": rows});
+    });
+});
+
+app.get('/Reports', (req, res) => {
+    res.render('Reports');
+});
+
+app.post('/updateCustomer', (req, res) => {
+    console.log('Got body:', req.body);
+    con.query("SELECT * FROM customers WHERE Cus_ID = '" + req.body.dropDown + "'", (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('updateCustomer', {"data": rows});
+    });
+});
+
+app.post('/updateCustomerAction', (req, res) => {
+    console.log('Got body:', req.body);
+    res.redirect('/ManageCustomers');
+});
+
+app.post('/updateEmployee', (req, res) => {
+    console.log('Got body:', req.body);
+    con.query("SELECT * FROM employees WHERE Emp_ID = '" + req.body.dropDown + "'", (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('updateEmployee', {"data": rows});
+    });
+});
+
+app.post('/updateEmployeeAction', (req, res) => {
+    console.log('Got body:', req.body);
+    res.redirect('/ManageEmployees');
+});
+
+app.post('/updateProduct', (req, res) => {
+    console.log('Got body:', req.body);
+    con.query("SELECT * FROM products WHERE product_ID = '" + req.body.dropDown + "'", (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('updateProduct', {"data": rows});
+    });
+});
+
+app.post('/updateProductAction', (req, res) => {
+    console.log('Got body:', req.body);
+    res.redirect('/ManageProducts');
+});
+
+app.post('/updateSteamerOrder', (req, res) => {
+    console.log('Got body:', req.body);
+    con.query("SELECT * FROM glove_steaming WHERE glove_steaming_ID = '" + req.body.dropDown + "'", (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('updateSteamerOrder', {"data": rows});
+    });
+});
+
+app.post('/updateSteamerOrderAction', (req, res) => {
+    console.log('Got body:', req.body);
+    res.render('/Manage_Steam_Orders');
+});
+
+app.get('/viewCustomers', (req, res) =>{
+    con.query('SELECT * FROM customers', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('viewCustomers', {"customers": rows});
+    });
+});
+
+app.get('/viewEmployees', (req, res) =>{
+    con.query('SELECT * FROM employees', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('viewEmployees', {"employees": rows});
+    });
+});
+
+app.get('/viewProducts', (req, res) =>{
+    con.query('SELECT * FROM products', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('viewProducts', {"products": rows});
+    });
+});
+
+app.get('/viewSales', (req, res) =>{
+    con.query('SELECT * FROM sales', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+
+        res.render('viewSales', {"sales": rows});
+    });
+});
+
+app.listen(3000);
